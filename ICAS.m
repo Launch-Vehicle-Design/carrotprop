e7b6cf4988a7c145eea9bd378d12686051829e1c
Nathan Rand <nrand@Nathans-MacBook-Pro.local>
2023-09-26 16:08:56 -0400

    Got the initial functional structure and airbreathing working for a configurable rocket structure (just for approximating the initial mass (mi)).

--- ICAS.m
+++ ICAS.m
@@ -7,41 +7,46 @@ clc;
 set(groot,'defaultAxesFontSize',14);
 set(groot, 'DefaultLineLineWidth', 2);
 
-%% Three Stage (Solid -> Solid -> Solid)
-clear;
-fprintf("Three Stage (Solid -> Solid -> Solid) Analysis: \n---------------------------------------------------------\n\n");
+%Define our inputs
+stages = ["Air Breathing", "Rocket"];
+Isp = [-1, 300];
+alpha = [1/2, 1/4];
+pi_e = [0.12, 0.12];
+DDeF_ratio = [0, 0];
+eta_o = [0.5, -1];
+hpr = [120000e3, -1];
+mp = 45.4;
 
-%Define basic givens for the conceptual problem
-ro = 6378e3; % (m) Radius of Earth
-ri = (6378+12.192)*1e3; % (m)
-rf = (6378+251.46)*1e3; % (m)
-go = 9.81; % (m/s^2)
-DDeF_ratio = 0; % From (Heiser and Pratt) - rough approximation
-Isp1 = 280; % All 3 stages (rough guess)
-Isp2 = Isp1;
-Isp3 = Isp1;
-% Initial guess values for alphas and pi_e
-alpha1 = 1/9;
-alpha2 = 1/9;
-alpha3 = 1/9;
-pi_e = 0.12;
-mp = 45.4; % (kg)
+%Run our analysis script
+disp(vehicle_mass_analysis(stages, Isp, alpha, pi_e, DDeF_ratio, eta_o, hpr, mp));
 
-lambda1 = sqrt(go*ro)/(go*Isp1*(1-DDeF_ratio));
-fprintf("Lambda 1 is: %1.3f \n", lambda1);
-lambda2 = sqrt(go*ro)/(go*Isp2*(1-DDeF_ratio));
-fprintf("Lambda 2 is: %1.3f \n", lambda2);
-lambda3 = sqrt(go*ro)/(go*Isp3*(1-DDeF_ratio));
-fprintf("Lambda 3 is: %1.3f \n", lambda3);
+function [mi] = vehicle_mass_analysis(stages, Isp, alpha, pi_e, DDeF_ratio, eta_o, hpr, mp)
+    %Define basic givens for the conceptual problem
+    ro = 6378e3; % (m) Radius of Earth
+    ri = (6378+12.192)*1e3; % (m)
+    rf = (6378+251.46)*1e3; % (m)
+    go = 9.81; % (m/s^2)
 
-gamma1 = 1/(exp(-sqrt(alpha1)*lambda1)-pi_e);
-fprintf("Gamma 1 is: %1.3f \n", gamma1);
-gamma2 = 1/(exp(-sqrt(alpha2)*lambda2)-pi_e);
-fprintf("Gamma 2 is: %1.3f \n", gamma2);
-gamma3 = 1/(exp(-sqrt(alpha3)*lambda3)-pi_e);
-fprintf("Gamma 3 is: %1.3f \n", gamma3);
-gamma = gamma1*gamma2*gamma3;
-fprintf("Overall Gamma: %3.1f \n", gamma);
-mi1 = gamma*mp;
-fprintf("Initial mass of the vehicle must be (mi1): %4.1f kg \n", mi1);
+    %Define memory space for our results to be stored for each stage
+    lambda = zeros(length(stages));
+    gamma = zeros(length(stages));
 
+    %Iterate over all of our stages
+    for i = 1:length(stages)
+        %Compute results for each stage
+        if upper(stages(i)) == "ROCKET"
+            lambda(i) = sqrt(go*ro)/(go*Isp(i)*(1-DDeF_ratio(i)));
+            gamma(i) = 1/(exp(-sqrt(alpha(i))*lambda(i))-pi_e(i));
+        elseif upper(stages(i)) == "AIR BREATHING"
+            lambda(i) = (go*ro*(1-(1/2)*(ro/rf)))/(eta_o(i)*hpr(i)*(1-DDeF_ratio(i)));
+            gamma(i) = 1/(exp(-alpha(i)*lambda(i))-pi_e(i)); 
+        else
+            disp("Non-Supported Stage Identifier Passed. Please pass either ROCKET or AIR BREATHING keys.");
+        end
+    end
+
+    %Determine cumulative results for the entire vehicle
+    gamma_tot = prod(gamma);
+    gamma_tot = gamma_tot(1);
+    mi = gamma_tot*mp;
+end
